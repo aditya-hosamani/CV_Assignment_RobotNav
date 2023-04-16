fd1eacbf61dc28b7ed275d13b043893a76dd3177
ainoal <77739788+ainoal@users.noreply.github.com>
2023-04-16 18:32:47 +0300

    call find_obj from move_block

--- src/main.m
+++ src/main.m
@@ -12,6 +12,9 @@
 % squareSize (in mm) and boardSize (#corners) are optional, and can either
 % be detected automatically from the images or provided explicitly by user
 [projMatrix, camParams] = calibrate(imgs, squareSize, boardSize);
+img = imread("test_images\images\img1.png")
+blocks = {"green","blue","red"};
+[]=move_Block(blocks,img,projMatrix,camParams)
 
 % Object detection
 
