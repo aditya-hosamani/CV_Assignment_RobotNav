f059ea8c2d6b70ed8128a3673d358bed888911e1
ainoal <77739788+ainoal@users.noreply.github.com>
2023-04-19 13:50:02 +0300

    Fixes to find objects and move blocks

--- src/main.m
+++ src/main.m
@@ -12,17 +12,17 @@ close all; clear; clc;
 
 % Perform camera calibration
 % Read calibration image
-calibImg = imread("..\test_images\calibration\img3.png");
+calibImg = imread("test_images\calibration\img3.png");
 % Number of squares along rows and columns
 boardSize = [6,9];
 % Size of each square in mm
 squareSize = 45;
 [projMatrix, camParams] = calibrate(calibImg, squareSize, boardSize);
 
-% img = imread("test_images\images\img1.png");
+img = imread("test_images\new_robot_cover\img0.png");
 % img = imread("test_images/images/img1.png");
-% blocks = ["green","blue","red"];
-% x=move_block(blocks,img,0,0);
+blocks = ["green","blue","red"];
+x=move_block(blocks,img,projMatrix,camParams);
 
 % Object detection
 
