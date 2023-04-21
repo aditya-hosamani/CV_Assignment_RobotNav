36bf66a185c58984c8c35eb3b70d6e93dffcc6ef
Aditya Hosamani <aditya.hosamani@student.lut.fi>
2023-04-21 11:49:47 +0300

    Calibration made manual

--- src/calibrate.m
+++ src/calibrate.m
@@ -4,135 +4,61 @@
 %  Author: Aditya Hosamani (0585939)	        |
 % ===============================================
 
-function [projMat, camParams] = calibrate(img, squareSize, boardSize)
-    % CALIBRATE Calibrate the camera using images of a scene.
+function [projM] = calibrate(inputImg)
+    % CALIBRATE     :   Calibrate the camera using a single input image
     % Arguments:
-    %           img  - An array of images to be used for calibration
-    %     squareSize - The size (mm) of the edge of each square of checkerboard
-    %      boardSize - The number of corners in chessboard pattern [rows, cols]
+    %      inputImg - The calibration image
     %
     % Returns:
     %        projMat - The projection matrix of the camera
-    %      camParams - The intrinsic and extrinsic camera parameters
 
-    % Define the size of the chessboard
-    if nargin < 3
-        squareSize = 25; % in millimeters
-        boardSize = [6,9]; % number of corners in the chessboard pattern
-    end
+    points2d = get2dUserInput(inputImg);
+    points3d = generateWorldPoints();
 
+    %save points2dTest points2d;
+    %save points3dTest points3d;
     
-    % smooth the image to reduce noise
-    smoothImg = imgaussfilt(img, 0.3);
-
-    % convert to grayscale
-    gray = rgb2gray(smoothImg);
-
-    maskedImg = imsharpen(imageMasker(gray));
-    %imwrite(maskedImg, "maskedImg.jpg")
+    %load points2d.mat;
+    %load points3d.mat;
+    %points2d = points2d';
+    %points3d = points3d';
     
-    % Generate the world coordinates of the corners of the chessboard pattern
-    worldPoints = generateWorldPoints(boardSize, squareSize);
-    
-    % Create an empty matrix to store the image points
-    imagePoints = [];
-    
-    % Detect the corners of the chessboard pattern in the image
-    corners = harris_corner_detector(maskedImg, 15.0, 1100);
-    
-    % ---------------------The below is done for img3.png to counter the corner ordering ----------------
-    temp = corners(35,:);
-    corners(35,:) = corners(36,:);
-    corners(36,:) = temp;
-    % ----------------------------------------------------------------------------------------------------
+    projM = performDLT(points3d, points2d);
 
-    %save corners.mat corners;
-    % Display the corners on the original image
-    %imshow(img);
-    %hold on;
-    %plot(corners(:, 1), corners(:, 2), 'r+', 'MarkerSize', 10);
-    %title("Detected " + size(corners,1) + " corners");
-
-    % If all checkerboard corners are not found, exit the function
-    isFound = false;
-    nCorners = (boardSize(1)-1) * (boardSize(2)-1);
-    if size(corners,1) ~= nCorners
-        disp("Failed to detect the required checkerboard corners only. Please provide a new image");
-        projMat = -1;
-        camParams = -1;
-    end
-
-    % If the corners are found, add them to the imagePoints matrix
-    imagePoints = [imagePoints; corners];
-    %save imagePoints imagePoints;
+    % Verify projection matrix by displaying the projected points
+    projPoints2d = project3d_2d(points3d, projM);
+    f2 = figure;
+    imshow(inputImg);
+    hold on;
+    plot(points2d(2,:), points2d(1,:), 'r+', 'MarkerSize', 10);
+    plot(projPoints2d(2,:), projPoints2d(1,:), 'bo', 'MarkerSize', 10);
     
-    % Calculate the intrinsic and extrinsic camera parameters
-    % Reshape the points to 2xn and 3xn
-    imagePoints = imagePoints';
-    worldPoints = worldPoints';
-    save worldPoints worldPoints;
-    save imagePoints imagePoints;
-    projMat = estimateProjMat(imagePoints, worldPoints);
-    camParams = QR_decomp(projMat);
 end
 
 
-function mImg = imageMasker(inputImg)
-    % Masks the input image such that the background clutter is minimized
-    % leaving better visiblity for the checkerboard
-    %
-    %   Arguments:
-    %       inputImg    - Grayscale input image
-    %
-    %   Returns:
-    %       mImg        - Grayscale masked image
-    %
-    
-    % Assuming the calibration target would be kept on the table, the
-    % table region is manually marked from the sample images.
-    % ROI boundaries: TL, TR, BR, BL
-    %maskROI = [[0,300], [1500,300], [1920,1080], [0,1080]];
-    mImg = inputImg;
-    % make the top black
-    mImg(1:400, 1:1920) = 0;
-    % make the left-edge black
-    mImg(400:800, 1:400) = 0;
-end
-
-
-function [worldPoints] = generateWorldPoints(boardSize, squareSize)
-    %GENERATEWORLDPOINTS Generates 3D world points for a checkerboard pattern
-    %
-    %   Arguments:
-    %       boardSize  - [rows, cols] size of the checkerboard pattern
-    %       squareSize - scalar size of each checkerboard square in mm
-    %
-    %   Returns:
-    %       worldPoints - (rows * cols, 3) matrix of 3D world points
-    %
-    
-    % Initialize worldPoints matrix
-    numPoints = (boardSize(1)-1) * (boardSize(2)-1);
-    worldPoints = zeros(numPoints, 3);
-    
-    % Generate 3D world points
-    [X, Y] = meshgrid(0:(boardSize(2)-2), 0:(boardSize(1)-2));
-    worldPoints(:, 1) = X(:) * squareSize;
-    worldPoints(:, 2) = Y(:) * squareSize;
-    worldPoints(:, 3) = 0;
-end
-
-
-function [projM]  = estimateProjMat(points2d, points3d)
-    % Function implementing DLT (Direct-Linear-Transform) to find and return
-    % a suitable projection matrix to transform points3d to points2d
-    % Arguments:
-    %       points2d - 2D vector of image points
-    %       points3d - 3D vector of world points
+function [points3d] = generateWorldPoints()
+    % GENERATEWORLDPOINTS   :   Generate world points for the cube corners
     %
     % Returns:
-    %         projM - Projection matrix
+    %        points3d       -   The 3D world points (3xM)
 
+    points3d = [ [0,0,50]; [0,0,0]; [315,0,0]; [315,0,50]; [360,0,50]; ...
+        [360,0,0]; [510,180,0]; [510,180,50] ];
+    points3d = points3d';
+
+end
+
+
+
+function [projM] = performDLT(points3d, points2d)
+    % CALIBRATE     :   Find projection matrix using the 3D world points
+    %                   and 2D image points
+    % Arguments:
+    %    points2d   -   2D image points (2xM)
+    %    points3d   -   3D world points (3xM)
+    %
+    % Returns:
+    %        projM  -   The projection matrix of the camera
 
     % Convert points to homogenous form
     nPoints = size(points3d,2);
@@ -140,20 +66,21 @@ function [projM]  = estimateProjMat(points2d, points3d)
     points3d = [points3d; ones(1,nPoints)];
     
     % Normalize the points
-    [normPoints2D, T_2D, normPoints3D, T_3D] = normalize(points2d, points3d);
-    %save normPoints2D normPoints2D
-    %save normPoints3D normPoints3D
+    [T_2d, normPoints2d] = normalize2D(points2d);
+    points2d = normPoints2d;
+    [T_3d, normPoints3d] = normalize3D(points3d);
+    points3d = normPoints3d;
 
     % Compute A for Am = 0
     n = 12;
     A = zeros(2*nPoints, n);
 
     for i=1:nPoints
-        u = normPoints2D(1,i);
-        v = normPoints2D(2,i);
-        w = normPoints2D(3,i);
+        u = points2d(1,i);
+        v = points2d(2,i);
+        w = points2d(3,i);
 
-        x = normPoints3D(:,i);
+        x = points3d(:,i);
 
         Ai =    [ 0, 0, 0, 0,   -w * x',   v * x' ;
                 w * x',   0, 0, 0, 0,  -u * x' ];
@@ -161,79 +88,147 @@ function [projM]  = estimateProjMat(points2d, points3d)
         A = [A; Ai];
     end
 
-
     % Use SVD to compute the projection matrix from A
     [~,~,V] = svd(A);
-    m = V(:,end);
-    h = m/m(end);
-    projM = reshape(h, [4,3])';
+    h = V(:,n);
+    h = h/h(n);
+    projM = reshape(h,4,3)';
 
     % Denormalize the projection matrix
-    projM = inv(T_2D) * projM * T_3D;
+    projM = inv(T_2d) * projM * T_3d;
     projM = projM/ projM(3,4);
 
 end
 
 
-function [normPoints2D, T_2D, normPoints3D, T_3D] = normalize(points2D, points3D)
-    % normalize     -   Normalize 2D and 3D image and world points
+function imgPoints = get2dUserInput(inputImg)
+    % get2dUserInput:   Get the 2D image points from user-clicks
     % Arguments:
-    %    points2D   -   Image points (2xN)
-    %    points3D   -   World points (3xN)
+    %      inputImg -   The calibration image
     %
     % Returns:
-    %  normPoints2D -   2D vector containing the detected image points
-    %          T_2D -   Transformation matrix for 2D vector normalization
-    %  normPoints3D -   3D vector containing the generrated world points
-    %          T_3D -   Transformation matrix for 3D vector normalization
+    %      imgPoints -  An array of 2xM image points
 
-    n = size(points2D, 2);
-    % normalize image points
-    u = points2D(1, :);
-    v = points2D(2, :);
-    dist = mean(sqrt((u - mean(u)) .* (u - mean(u)) + (v - mean(v)) .* (v - mean(v))));
-    T_2D = [sqrt(2) / dist  0            -sqrt(2) * mean(u) / dist;
-         0            sqrt(2) / dist  -sqrt(2) * mean(v) / dist;
-         0            0            1];
-    normPoints2D = [u; v; ones(1, n)];
-    normPoints2D = T_2D * normPoints2D;
+    % Get user input
+    imgPoints = [];
+    nPoints = 8;
     
-    % normalize world points
-    X = points3D(1, :);
-    Y = points3D(2, :);
-    Z = points3D(3, :);
-    dist = mean(sqrt((X - mean(X)) .* (X - mean(X)) + (Y - mean(Y)) .* (Y - mean(Y)) + (Z - mean(Z)) .* (Z - mean(Z))));
-    T_3D = [sqrt(3) / dist  0               0       -sqrt(3) * mean(X) / dist;
-                0       sqrt(3) / dist      0       -sqrt(3) * mean(Y) / dist;
-                0           0       sqrt(3) / dist  -sqrt(3) * mean(Z) / dist;
-                0           0               0                1];
-    normPoints3D = [X; Y; Z; ones(1, n)];
-    normPoints3D = T_3D * normPoints3D;
+    f1 = figure;
+    imshow(inputImg); hold on;
+    for ptIdx = 1:nPoints
+        coordinates_input = ginput(1);
+        row = round(coordinates_input(2));
+        column = round(coordinates_input(1));
+        fprintf('You clicked on pixel in row %d, column %d\n', ...
+           row, column);
+        imgPoints = [imgPoints; [row, column]];
+        %plot(row, column, 'g*', 'MarkerSize', 10);
+    end
+    imgPoints = imgPoints';
+    close(f1);
 
 end
 
 
-function [params] = QR_decomp(projM)
-    % QR_decomp  -   QR decomposition to find the intrinsic and
-    % extrinsic camera parameters from the projection matrix M
+function [T_2D, normPoints2d] = normalize2D(inPoints)
+    % NORMALIZE2D   :   Normalize 2D image points
     % Arguments:
-    %    projM   -   Projection matrix of size 3x4
+    %    points2D   -   Image points
     %
     % Returns:
-    %    params  -   A struct containing the intrinsic and extrinsic camera
-    %    parameters
+    %  normPoints2D -   2D vector containing the detected image points
+    %          T_2D -   Transformation matrix for 2D vector normalization
+
+    % initialize T
+    Tt = eye(3);
+    % calculate centroid -> translation component known
+    t = [-mean(inPoints(1,:)), -mean(inPoints(2,:))];
+    Tt(1:2,3) = t;
+    
+    % translate points
+    inPoints = Tt*inPoints;
+
+    % compute scaling -> scaling component of T known
+    % avgdistance from origin = 1/m * sum(norm(x)), where m = # points
+    % compute norm of each vector
+    nx = sqrt(sum(inPoints(1:2,:).^2));
+    % we want avgdist_from_O = s * (1/m) * sum(norm(x)) = sqrt(2) 
+    % <-> s = sqrt(2) / (1/m) * sum(nx) = sqrt(2) / mean(nx)
+    s = sqrt(2) / mean(nx);
+    
+    % construct T from t and s
+    Ts = [s 0 0
+         0 s 0 
+         0 0 1];
+    % translation followed by scaling
+    T_2D = Ts*Tt;
+    normPoints2d = Ts * inPoints;
+
+end
+
+
+function [T_3D, normPoints3d] = normalize3D(inPoints)
+    % NORMALIZE3D   :   Normalize 3D image points
+    % Arguments:
+    %    inPoints   -   3D World points
     %
+    % Returns:
+    %  normPoints3d -   3D vector containing the detected image points
+    %          T_3D -   Transformation matrix for 3D vector normalization
 
-    H = projM(:,1:3);
-    h = projM(:,end);
+    % initialize T
+    Tt = eye(4);
+    % calculate centroid -> translation component known
+    t = [-mean(inPoints(1,:)), -mean(inPoints(2,:)), -mean(inPoints(3,:))];
+    Tt(1:3,4) = t;
+    
+    % translate points
+    inPoints = Tt*inPoints;
+    % compute scaling -> scaling component of T known
+    
+    % avgdistance from origin = 1/m * sum(norm(x)), where m = # points
+    % compute norm of each vector
+    nx = sqrt(sum(inPoints(1:3,:).^2));
+    % we want avgdist_from_O = s * (1/m) * sum(norm(x)) = sqrt(2) 
+    % <-> s = sqrt(2) / (1/m) * sum(nx) = sqrt(2) / mean(nx)
+    s = sqrt(3) / mean(nx);
+    
+    % construct T from t and s
+    Ts = [s 0 0 0
+         0 s 0 0
+         0 0 s 0
+         0 0 0 1];
+    % translation followed by scaling
+    T_3D = Ts*Tt;
+    normPoints3d = Ts * inPoints;
 
-    [R_trans, K_inv] = qr(inv(H));
+end
 
-    params.R = R_trans';
-    params.K = inv(K_inv);
 
-    % Calculating camera position center
-    C = -inv(H)*h;
-    params.t = -params.R*C;
+function [projPoints2d] = project3d_2d(points3d, projM)
+    % PROJECT3D_2D  :   Find the 2D projections of 3D world points using the
+    %                   projection matrix
+    % Arguments:
+    %    points3d   -   3D World points (3xM)
+    %    projM      -   Projection matrix (3x4)
+    %
+    % Returns:
+    %  projPoints2d -   2D projections of 3D world points
+
+    nPoints = size(points3d,2);
+    tmp = ones(nPoints,1)';
+    projected_points = zeros(nPoints,3,1);
+    %disp(size(projected_points));
+    %disp(size(tmp));
+    homogenous_points = [points3d; tmp];
+    %disp(size(homogenous_points));
+    projected_points =  projM * homogenous_points;
+    %disp(size(projected_points));
+
+    projPoints2d = zeros(2,size(points3d,2));
+
+    projPoints2d(1,:) = projected_points(1,:,:) ./ projected_points(3,:,:);
+    projPoints2d(2,:) = projected_points(2,:,:) ./ projected_points(3,:,:);
+
 end
 
--- src/main.m
+++ src/main.m
@@ -1,7 +1,6 @@
 % ===============================================
 %  Course: Computer Vision (BM40A901)	        |
 %  Practical Assignment: Collecting Cubes		|
-%  Author: Aditya Hosamani (0585939)	        |
 % ===============================================
 
 % Close open windows, clear workspace and console
@@ -11,54 +10,16 @@ close all; clear; clc;
 % TBD
 
 % Perform camera calibration
-% Temporary calibration using MATLAB in-built
-calibPath = "test_images\calibration";
-[camParams, projMatrix]= calibrateMatLocal(calibPath);
+calibImg = imread("..\test_images\calibration\img0.png");
+% points3d = 0.01 * points3d;     % convert cm to m
+projMatrix = calibrate(calibImg);
 
-%cameraParams = dltCalibration(calibImg, squareSize);
 
-img = imread("test_images\new_robot_cover\imgx.png");
-% img = imread("test_images/images/img1.png");
-blocks = ["green","blue","red"];
-x=move_block(blocks,img,projMatrix,camParams);
+% img = imread("test_images\new_robot_cover\imgx.png");
+% % img = imread("test_images/images/img1.png");
+% blocks = ["green","blue","red"];
+% x=move_block(blocks,img,projMatrix,camParams);
 
 % Object detection
 
 % Move the robot
-
-
-%% Auxiliary functions
-function [params, P] = calibrateMatLocal(calibPath)
-    fileList = dir(calibPath);
-    % Filter the list to only include image files
-    imageFileNames = {};
-    for i = 1:length(fileList)
-        [~, fileName, ext] = fileparts(fileList(i).name);
-        if strcmpi(ext, '.png')
-            filePath = fullfile(calibPath, fileList(i).name);
-            imageFileNames{end+1,1} = char(imageDatastore(filePath).Files);
-        end
-    end
-    
-    % Detect calibration pattern.
-    [imagePoints, boardSize] = detectCheckerboardPoints(imageFileNames);
-    
-    % Generate world coordinates of the corners of the squares.
-    squareSize = 45; % millimeters
-    worldPoints = generateCheckerboardPoints(boardSize, squareSize);
-    
-    % Calibrate the camera.
-    imageSize = [1080, 1920];
-    [params, ~, ~] = estimateCameraParameters(imagePoints, worldPoints, ...
-                                         ImageSize=imageSize);
-
-
-    intrinsic = params.Intrinsics;
-    disp(size(imagePoints));
-    meanImgPoints = mean(imagePoints,3);
-
-    extrinsic = estimateExtrinsics(meanImgPoints, worldPoints, intrinsic);
-    P = cameraProjection(intrinsic, extrinsic);
-
-end
-
